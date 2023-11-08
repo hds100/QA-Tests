8428da5f0d16b34180add77480839b9bc440a19f
João Pedro H <148555673+hds100@users.noreply.github.com>
2023-11-08 08:51:00 -0300

    Update Python simple unit test.py

--- Python simple unit test.py
+++ Python simple unit test.py
@@ -7,7 +7,7 @@ import unittest
 def add_numbers(a, b):
     return a + b
 
-#  implementing the test class and testing the function in different cases
+#  Implementing the test class and testing the function in different cases
 
 class TestAddNumbers(unittest.TestCase):
 
